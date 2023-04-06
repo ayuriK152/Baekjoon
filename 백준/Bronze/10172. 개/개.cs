707c1e976e1d81817a125b797149a23f266aaa6b
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-04-07 01:31:58 +0900

    [Bronze V] Title: 개, Time: 52 ms, Memory: 5344 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+
+namespace AlgorithmStudy
+{
+    class boj10172
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+            Console.WriteLine(sb);
+        }
+    }
+}
