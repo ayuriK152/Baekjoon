2b9b1f74786598106894136ba8db6b77395d1d94
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-04-11 02:15:00 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2300 ms, Memory: 20088 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cs
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Text;
 
 namespace AlgorithmStudy
 {
@@ -8,17 +7,18 @@ namespace AlgorithmStudy
     {
         public static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
             int[] counting = new int[10000];
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(sr.ReadLine());
             for (int i = 0; i < n; i++)
-                counting[short.Parse(Console.ReadLine()) - 1]++;
-            StringBuilder sb = new StringBuilder();
+                counting[short.Parse(sr.ReadLine()) - 1]++;
             for (short i = 0; i < 10000; i++)
                 if (counting[i] != 0)
                     for (int j = 0; j < counting[i]; j++)
                         sw.WriteLine(i + 1);
             sw.Close();
+            sr.Close();
         }
     }
 }
