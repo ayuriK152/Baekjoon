a77aa02760440db0c619fdca9af85f7e29575a0b
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-04-15 20:37:53 +0900

    [Bronze V] Title: 사분면 고르기, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    class boj14681
+    {
+        public static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x > 0 && y > 0)
+                Console.WriteLine("1");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("2");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("3");
+            else if (x > 0 && y < 0)
+                Console.WriteLine("4");
+        }
+    }
+}
