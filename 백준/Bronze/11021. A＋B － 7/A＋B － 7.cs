a51deb558dbe15ee2ec2fa4a4cd3fb9bb84d17eb
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-04-20 23:20:58 +0900

    [Bronze V] Title: A+B - 7, Time: 68 ms, Memory: 6188 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace AlgorithmStudy
+{
+    class boj11021
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StringBuilder sb = new StringBuilder();
+
+            int t = int.Parse(sr.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] input = sr.ReadLine().Split();
+                sb.AppendLine($"Case #{i + 1}: " + (int.Parse(input[0]) + int.Parse(input[1])).ToString());
+            }
+            sr.Close();
+            Console.WriteLine(sb);
+        }
+    }
+}
