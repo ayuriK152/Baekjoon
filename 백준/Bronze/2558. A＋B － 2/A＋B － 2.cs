653af6c9b0bfdb47629194505afc7a87498ba299
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-04-24 23:27:24 +0900

    [Bronze V] Title: A+B - 2, Time: 88 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    class boj2558
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
