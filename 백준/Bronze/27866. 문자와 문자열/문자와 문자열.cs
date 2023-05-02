dbd890e0569ae6be022b1de3c9ea9c675c430176
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-05-02 19:48:10 +0900

    [Bronze V] Title: 문자와 문자열, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    class boj27866
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(input[n - 1]);
+        }
+    }
+}
