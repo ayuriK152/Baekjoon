5ca5762e500145675f9cf0f22f6b25b23bbe9e4c
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-05-02 19:40:10 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 64 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    class boj11718
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == null)
+                    break;
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
