619093a126ef7ded2744d71840f61ce805c684fb
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-05-08 01:08:18 +0900

    [Silver V] Title: 창문 닫기, Time: 60 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    class boj13909
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((int)Math.Sqrt(n));
+        }
+    }
+}
