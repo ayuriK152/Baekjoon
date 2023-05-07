9ebcbf10ee74cddeef19a397b2c81a6764bd1e22
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-05-07 09:47:41 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AlgorithmStudy
+{
+    internal class boj25083
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("         ,r\'\"7");
+            Console.WriteLine("r`-_   ,\'  ,/");
+            Console.WriteLine(" \\. \". L_r\'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.Write("      |");
+        }
+    }
+}
