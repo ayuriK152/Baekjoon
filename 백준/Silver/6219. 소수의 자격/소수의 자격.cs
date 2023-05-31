a47595ec2b2d10e3ab199dc154eb2289ec575d49
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-05-31 19:30:00 +0900

    [Silver III] Title: 소수의 자격, Time: 100 ms, Memory: 11408 KB -BaekjoonHub

---
+++ 백준/Silver/6219. 소수의 자격/소수의 자격.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmStudy
+{
+    class boj6219
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]), b = int.Parse(input[1]), d = int.Parse(input[2]);
+            bool[] primeCheck = new bool[b];
+            List<int> primeNums = new List<int>();
+
+            for (int i = 1; i < b; i++)
+            {
+                if (!primeCheck[i])
+                {
+                    primeCheck[i] = true;
+                    if (i + 1 >= a)
+                        primeNums.Add(i + 1);
+                    for (int j = i + 1; j <= b; j += i + 1)
+                    {
+                        primeCheck[j - 1] = true;
+                    }
+                }
+            }
+
+            int count = 0;
+            foreach (int i in primeNums)
+            {
+                int temp = i;
+                while (temp != 0)
+                {
+                    if (temp % 10 == d)
+                    {
+                        count++;
+                        break;
+                    }
+                    temp /= 10;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
