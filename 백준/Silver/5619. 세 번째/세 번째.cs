c358184efd80ee26cb3bdbab30f0a9469ce776b8
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-08-21 21:15:28 +0900

    [Silver II] Title: 세 번째, Time: 72 ms, Memory: 6492 KB -BaekjoonHub

---
+++ 백준/Silver/5619. 세 번째/세 번째.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmStudy
+{
+    class boj5619
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+            Array.Sort(arr);
+            List<int> selected = new List<int>();
+            selected.Add(arr[0]); selected.Add(arr[1]); selected.Add(arr[2]);
+            if (n > 3)
+                selected.Add(arr[3]);
+            int[] result = n > 3 ? new int[12] : new int[6];
+            int index = 0;
+            for (int i = 0; i < selected.Count; i++)
+            {
+                for (int j = 0; j < selected.Count; j++)
+                {
+                    if (i == j)
+                        continue;
+                    result[index++] = int.Parse($"{selected[i]}{selected[j]}");
+                }
+            }
+            Array.Sort(result);
+            Console.Write(result[2]);
+        }
+    }
+}
