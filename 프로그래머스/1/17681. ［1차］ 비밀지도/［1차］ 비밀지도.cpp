e87a033b091508bea22d214e7f242ae6d05732d2
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2023-11-17 05:13:25 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
@@ -8,12 +7,13 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     vector<string> answer;
     for (int i = 0; i < n; i++) {
         string temp = "";
-        for (int j = n - 1; j >= 0; j--) {
-            int current = pow(2, j);
+        int current = 1 << n - 1;
+        for (int j = 0; j < n; j++) {
             if ((arr1[i] & current) == current || (arr2[i] & current) == current)
                 temp.append("#");
             else
                 temp.append(" ");
+            current = current >> 1;
         }
         answer.push_back(temp);
     }
