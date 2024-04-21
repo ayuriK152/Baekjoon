336178fb953ed76d973bc233e448d55fd58f1b8f
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2024-04-22 03:46:10 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string input;
+	getline(cin, input);
+	for (int i = 0; i < input.size(); i++) {
+		cout << input[i];
+		if ((i + 1) % 10 == 0)
+			cout << endl;
+	}
+	return 0;
+}
