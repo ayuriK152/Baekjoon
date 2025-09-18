6e14d83c31e1937369b24a667ea0a4f3992e44dc
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2025-09-18 11:47:37 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool flag[8];
+int n, m;
+vector<int> result;
+string resultStr;
+
+void Recursive(int num, int len) {
+	if (len == m - 1)
+	{
+		for (int i = 0; i < result.size(); i++)
+			resultStr.append(to_string(result[i]) + " ");
+		resultStr.append(to_string(num + 1) + "\n");
+		return;
+	}
+	for (int i = num; i < n; i++)
+	{
+		if (flag[i])
+			continue;
+		flag[i] = true;
+		result.push_back(num + 1);
+		Recursive(i, len + 1);
+		flag[i] = false;
+		result.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		flag[i] = true;
+		Recursive(i, 0);
+		flag[i] = false;
+	}
+
+	cout << resultStr;
+
+	return 0;
+}
