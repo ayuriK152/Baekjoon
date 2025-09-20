5b7c6885c1c64dfd610dbe6e0fb195a927be1afd
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2025-09-21 03:31:38 +0900

    [Gold V] Title: AC, Time: 68 ms, Memory: 4560 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,95 +1,71 @@
 #include <iostream>
 #include <sstream>
-
 using namespace std;
 
-class Deque {
-public:
-	string arr[100000];
-	int count, first, last;
-	bool isReverse;
-	bool isError;
-	Deque(int size) {
-		count = size;
-		first = 0;
-		last = size - 1;
-		isReverse = false;
-		isError = false;
-	}
-
-	void Reverse() {
-		isReverse = !isReverse;
-	}
-
-	void Delete() {
-		if (first > last || count == 0 || isError) {
-			isError = true;
-			return;
-		}
-		if (!isReverse) {
-			first++;
-			count--;
-		}
-		else {
-			last--;
-			count--;
-		}
-	}
-	
-	string ShowAll() {
-		string result;
-		if (isError)
-			result = "error";
-		else if (count == 0)
-			result = "[]";
-		else if (!isReverse) {
-			result += "[";
-			for (int i = first; i <= last; i++) {
-				result += arr[i] + ",";
-			}
-			result[result.length() - 1] = ']';
-		}
-		else {
-			result += "[";
-			for (int i = last; i >= first; i--) {
-				result += arr[i] + ",";
-			}
-			result[result.length() - 1] = ']';
-		}
-		return result;
-	}
-};
-
 int main() {
+	string answer;
+
 	int t;
 	cin >> t;
 
-	for (int i = 0; i < t; i++) {
-		string p;
-		cin >> p;
-
+	for (; t > 0; t--) {
+		int arr[100000];
 		int n;
-		cin >> n;
-		Deque deque(n);
+		string cmd, arrInput;
+		cin >> cmd >> n >> arrInput;
+		arrInput.erase(arrInput.begin());
+		arrInput.erase(arrInput.end() - 1);
 
-		string input;
-		cin >> input;
-		input = input.substr(1, input.length() - 2);
-		istringstream iss(input);
-
-		for (int j = 0; j < n; j++)
-			getline(iss, deque.arr[j], ',');
-		for (int j = 0; j < p.length(); j++) {
-			switch (p[j]) {
-			case 'R':
-				deque.Reverse();
-				break;
-			case 'D':
-				deque.Delete();
-				break;
+		int elementCount = 0, temp = 0;
+		for (int i = 0; i < arrInput.size(); i++) {
+			if (arrInput[i] == ',') {
+				arr[elementCount++] = temp;
+				temp = 0;
+			}
+			else {
+				temp *= 10;
+				temp += arrInput[i] - '0';
 			}
 		}
-		cout << deque.ShowAll() << endl;
+		arr[elementCount] = temp;
+
+		int left = 0, right = n - 1, count = n;
+		bool isFlip = false;
+		for (int i = 0; i < cmd.size(); i++) {
+			if (cmd[i] == 'R') {
+				isFlip = !isFlip;
+			}
+			else if (cmd[i] == 'D') {
+				if (count-- == 0)
+					break;
+				if (!isFlip)
+					left++;
+				else
+					right--;
+			}
+		}
+		string tempResult;
+		if (count < 0)
+			tempResult = "error\n";
+		else {
+			tempResult = "[";
+			if (count > 0) {
+				if (!isFlip)
+					for (int i = left; i <= right; i++)
+						tempResult.append(to_string(arr[i]) + ",");
+				else
+					for (int i = right; i >= left; i--)
+						tempResult.append(to_string(arr[i]) + ",");
+				tempResult.erase(tempResult.end() - 1);
+			}
+			tempResult.append("]\n");
+		}
+
+		answer.append(tempResult);
 	}
+
+	answer.erase(answer.end() - 1);
+	cout << answer;
+
 	return 0;
 }
