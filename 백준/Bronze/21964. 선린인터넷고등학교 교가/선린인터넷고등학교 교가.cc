e54c35dfcbc68d66b2e3ad63fb2c672e8842d1cd
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2025-10-08 23:24:18 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string a;
+    cin >> n >> a;
+    for (int i = n - 5; i < n; i++)
+        cout << a[i];
+    
+    return 0;
+}
