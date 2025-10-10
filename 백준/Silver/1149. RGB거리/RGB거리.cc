0c691427db4c9011d2a97dde28e00944af8f6de0
Won-seop Lee <77537017+ayuriK152@users.noreply.github.com>
2025-10-11 03:53:55 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,33 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int values[1000][3];
-int dp[1000][3];
-int n;
-
 int main() {
+	int dp[3][1000];
+	int arr[3][1000];
+
+	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int r, g, b;
-		cin >> r >> g >> b;
-		values[i][0] = r;
-		values[i][1] = g;
-		values[i][2] = b;
+	for (int i = 0; i < n; i++)
+		cin >> arr[0][i] >> arr[1][i] >> arr[2][i];
+
+	dp[0][0] = arr[0][0];
+	dp[1][0] = arr[1][0];
+	dp[2][0] = arr[2][0];
+
+	for (int i = 1; i < n; i++) {
+		dp[0][i] = arr[0][i] + min(dp[1][i - 1], dp[2][i - 1]);
+		dp[1][i] = arr[1][i] + min(dp[0][i - 1], dp[2][i - 1]);
+		dp[2][i] = arr[2][i] + min(dp[0][i - 1], dp[1][i - 1]);
 	}
 
-	dp[0][0] = values[0][0];
-	dp[0][1] = values[0][1];
-	dp[0][2] = values[0][2];
-
-	for (int i = 1; i < n; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			dp[i][j] = min(dp[i - 1][(j + 1) % 3], dp[i - 1][(j + 2) % 3]) + values[i][j];
-		}
-	}
-
-	cout << min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
+	cout << min(dp[0][n - 1], min(dp[1][n - 1], dp[2][n - 1]));
 
 	return 0;
 }
